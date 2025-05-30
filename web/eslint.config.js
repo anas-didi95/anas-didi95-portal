d24da75673f9463e01f825efac7949d5ef4535ee
Anas Juwaidi <anas.didi95@gmail.com>
2025-05-30 16:34:43 +0000

    MOD: Amend linter config as per suggestion

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -8,11 +8,15 @@ import eslintConfigPrettier from "eslint-config-prettier/flat";
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, tseslint.configs.stylisticTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
