3d97facc5e0c02708dc73a7f68fd9b9f3cd2d3d7
Anas Juwaidi <anas.didi95@tutamail.com>
2025-06-01 00:32:16 +0800

    Feature: Setup devcontainer and project (#1)
    * ADD: Add devcontainer java 21 and typescript
    
    * ADD: Change image to use microsoft official image
    
    * ADD: Add extensions
    
    * MOD: Amend positioning
    
    * ADD: Add micronaut starter
    
    * ADD: Add formatter
    
    * ADD: Add docker compose
    
    * ADD: Add cache folder for gradle
    
    * ADD: Add vite starter
    
    * ADD: Add formatter
    
    * MOD: Amend linter config as per suggestion
    
    * ADD: Add tanstack router
    
    * REM: Remove unused files
    
    * MOD: Amend script to forward host from container
    
    * MOD: Use hash history for router
    
    * ADD: Add static resource config
    
    * ADD: Add dockerfile for publish
    
    * ADD: Add codeowners
    
    * ADD: Add workflow to check for pr branch
    
    * ADD: Add workflow ci
    
    * ADD: Add step to change workdir
    
    * MOD: Amend cmd to debug
    
    * MOD: Amend cmd to debug
    
    * MOD: Amend cmd to debug
    
    * MOD: Amend cmd to debug
    
    * ADD: Add default workdir
    
    * MOD: Amend cmd to debug
    
    * ADD: Add cmd to debug download artifacts
    
    * ADD: Add step to prepare artifact and get dockerfile

---
+++ app/src/test/java/com/anasdidi/portal/PortalTest.java
@@ -0,0 +1,19 @@
+/* (C) Anas Juwaidi Bin Mohd Jeffry. All rights reserved. */
+package com.anasdidi.portal;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@MicronautTest
+class PortalTest {
+
+  @Inject EmbeddedApplication<?> application;
+
+  @Test
+  void testItWorks() {
+    Assertions.assertTrue(application.isRunning());
+  }
+}
