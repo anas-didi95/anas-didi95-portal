28e1f008348f0b00e039e9f16be10805180e575d
Anas Juwaidi <anas.didi95@gmail.com>
2025-07-06 01:12:38 +0000

    ADD: Setup native image build

--- app/src/main/java/com/anasdidi/portal/Application.java
+++ app/src/main/java/com/anasdidi/portal/Application.java
@@ -1,9 +1,21 @@
 /* (C) Anas Juwaidi Bin Mohd Jeffry. All rights reserved. */
 package com.anasdidi.portal;
 
+import ch.qos.logback.classic.jul.LevelChangePropagator;
+import com.anasdidi.portal.common.aspect.MaskingPatternLayout;
+import io.micronaut.core.annotation.ReflectionConfig;
+import io.micronaut.core.annotation.TypeHint.AccessType;
 import io.micronaut.runtime.Micronaut;
+import liquibase.change.core.RawSQLChange;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+@ReflectionConfig(
+    type = LevelChangePropagator.class,
+    accessType = AccessType.ALL_DECLARED_CONSTRUCTORS)
+@ReflectionConfig(
+    type = MaskingPatternLayout.class,
+    accessType = AccessType.ALL_DECLARED_CONSTRUCTORS)
+@ReflectionConfig(type = RawSQLChange.class, accessType = AccessType.ALL_DECLARED_METHODS)
 public class Application {
 
   public static void main(String[] args) {
