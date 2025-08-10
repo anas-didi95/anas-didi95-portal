2e2d0a7836e9086361f512921d3a75713431ff30
Anas Juwaidi <anas.didi95@gmail.com>
2025-08-10 08:18:07 +0000

    ADD: Add function to parse date value

--- web/src/commons/utils.ts
+++ web/src/commons/utils.ts
@@ -8,12 +8,13 @@ export const convertDateTimeInput = (dateStr?: string) => {
   }
 
   const date = new Date(dateStr);
+  const parseValue = (v: number): string => (v < 10 ? "0" + v : "" + v);
   const yyyy = date.getFullYear();
-  const MM = date.getMonth() + 1;
-  const dd = date.getDate();
-  const HH = date.getHours();
-  const mm = date.getMinutes();
-  return `${yyyy}-${MM < 10 ? "0" + MM : MM}-${dd < 10 ? "0" + dd : dd}T${HH}:${mm}`;
+  const MM = parseValue(date.getMonth() + 1);
+  const dd = parseValue(date.getDate());
+  const HH = parseValue(date.getHours());
+  const mm = parseValue(date.getMinutes());
+  return `${yyyy}-${MM}-${dd}T${HH}:${mm}`;
 };
 
 export const convertDateTimeLocale = (dateStr?: string) => {
